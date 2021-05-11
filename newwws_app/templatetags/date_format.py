92bc0cf44accfbc52469dfb5ba0a445c8e194b73
Gontrandddre <gontrandddre@gmail.com>
2021-05-11 13:38:08 +0200

    fix format date

--- newwws_app/templatetags/date_format.py
+++ newwws_app/templatetags/date_format.py
@@ -9,6 +9,9 @@ def date_format(value):
     try:
         format = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
     except ValueError:
-        format = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S+00:00")
+        try: 
+            format = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S+00:00")
+        except ValueError:
+             format = None
 
     return format
